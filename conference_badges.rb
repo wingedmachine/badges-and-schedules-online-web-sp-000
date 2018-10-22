d1e30c919ea19896d6adc678fbd6c6e376d98828
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-22 21:17:55 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,5 +19,7 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  attendees.each { |attendee| puts badge_maker(attendee) }
+  attendees.each do |attendee| 
+    puts badge_maker(attendee)
+    puts assign_rooms(attendee)
 end
